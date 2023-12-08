b9cc13d916d8738bbe4cb9ce4b548de9d2b5d214
Alexandru Jircan <alexandru.jircan@uipath.com>
2023-12-08 19:05:25 +0200

    make it kinda work

--- content.js
+++ content.js
@@ -4,43 +4,35 @@ async function kill(event){
     resp = await (await fetch(event.currentTarget.link)).text()
     let doc = new DOMParser().parseFromString(resp, "text/html");
     anchor = doc.querySelector("body > table > tbody > tr:nth-child(2) > td > a")
-    resp2 = await (await fetch(anchor.href)).text()
-    let doc2 = new DOMParser().parseFromString(resp2, "text/html");
-    body = doc2.querySelector("body")
-    info = body.getAttribute("onload")
-    link2 = info.substring(17, info.length - 1)
-    resp3 = await (await fetch(link2, { headers: {
-        "RequestMode": "no-cors",
-      }, })).text()
-    console.log( resp3 )
+    console.log(anchor.href)
 }
 
 const trs = document.querySelectorAll("body > table:nth-child(9) > tbody > tr");
-if(trs) {
+if(trs.length > 0) {
+    console.log(trs)
+    console.log("Home bby")
     for (let tr of trs) {
-        let td = document.createElement('td');
-        if( tr.className == "tr1" ){
-            let content = document.createTextNode("💀");
-            td.setAttribute("style", "text-align:center; vertical-align: middle;");
-            td.appendChild(content);
-            tr.appendChild(td);
-        }
-        else {
-            // > table > tbody > tr > td > a
-            let info = tr.querySelector("td:nth-child(8) > table > tbody > tr > td > a")
-            let anchor = document.createElement("a");
-            if( info != null && info.href ){
-                anchor.link = info.href.substring(24, info.href.length - 2);
-                anchor.addEventListener("click", kill)
-                let content = document.createElement("img");
-                content.src = chrome.runtime.getURL("download.png");
-                content.setAttribute("width", "20px");
-                td.setAttribute("style", "text-align:center; vertical-align: middle;");
-                anchor.appendChild(content);
+        let anchors = tr.querySelectorAll("td:nth-child(8) > table > tbody > tr > td > a")
+        for (let anchor of anchors){
+            if( anchor != null && anchor.href ){
+                anchor.href = anchor.href.substring(24, anchor.href.length - 2);
             }
-            td.appendChild(anchor);
-            tr.appendChild(td);
         }
     }
+}
 
+const iframe = document.querySelector("#pdf-js-viewer")
+if( iframe != null){
+    console.log("PDF bby")
+    console.log(iframe.src)
+    let regex = /file=(?:..\/..\/)?(.*?)&/;
+    let match = iframe.src.match(regex);
+    
+    if (match) {
+        console.log("yeah bby")
+        book_url = "https://www.bcucluj.ro/"+match[1]
+        console.log(book_url);
+        location.replace(book_url)
+    }
+    iframe.parentElement.removeChild(iframe)
 }
